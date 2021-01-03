d1e4f7ceb4115f36034a0061c62223b9f3103bc9
Andrew Massart <amassart@asu.edu>
2021-01-02 23:32:12 -0500

    created stand-alone routing module for auth routes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,17 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [ReactiveFormsModule, AngularFireAuthModule, SharedModule],
+  imports: [
+    ReactiveFormsModule,
+    AngularFireAuthModule,
+    SharedModule,
+    AuthRoutingModule,
+  ],
 })
 export class AuthModule {}
